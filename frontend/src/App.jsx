af7d3f33a55c3dec7db8eadd0eca206156d96ca9
Demo <fozirawat6869@gmail.com>
2026-07-22 01:38:56 +0530

    Resolve merge conflict in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,21 +7,13 @@ import Home from './pages/Home'
 function App() {
 
   return (
-<<<<<<< HEAD
     <>
-  <div className="bg-blue-500 text-white p-4">  mayur rawat </div>
-      </>
-=======
-    <Routes>
-      
-      {/*User routes  */}
-      <Route path="/" element={<Home />} />
-
-      {/* Admin routes */}
-      
-
-    </Routes>
->>>>>>> 9c56dd6a77bf00f18fbcc9d79222e3dfd947bd17
+      <div className="bg-blue-500 text-white p-4">mayur rawat</div>
+      <Routes>
+        {/* User routes */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   )
 }
 
