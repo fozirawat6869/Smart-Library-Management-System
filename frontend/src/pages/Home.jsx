9c56dd6a77bf00f18fbcc9d79222e3dfd947bd17
Sandeep Bisht <bishtsandeep833@gmail.com>
2026-07-21 22:08:06 +0530

    Add Sidebar component for navigation; implement DashboardLayout to include Sidebar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
-
+import Footer from '../components/Footer/Footer'
 const Home = () => {
   return (
     <>
       <Navbar />
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      <h1>Hello </h1>
+      
+      <Footer />
+      
     </>
   )
 }
