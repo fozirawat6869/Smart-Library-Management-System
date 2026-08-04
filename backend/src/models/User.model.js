570a40843e3817d1b7cc21178bd7c9842d4f1972
Demo <fozirawat6869@gmail.com>
2026-08-05 00:40:19 +0530

    fix: resolve merge conflicts in backend

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -70,7 +70,7 @@ userSchema.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model("User", userSchema);
 
 // const User = mongoose.model("User", userSchema);
 
